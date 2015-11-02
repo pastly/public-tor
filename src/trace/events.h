b9c93af1dc89f8575a095028d520e3926018a84a
David Goulet <dgoulet@ev0ke.net>
2015-11-02 15:15:57 -0500

    Improve
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- src/trace/events.h
+++ src/trace/events.h
@@ -29,7 +29,23 @@
 #define tor_trace(subsystem, name, args...) \
   tor_trace_shadow_##subsystem##_##name(args)
 
-#else /* TOR_TRACE_SHADOW */
+#elif TOR_TRACE_LOG_DEBUG /* TOR_TRACE_SHADOW */
+
+#include "torlog.h"
+
+/* Stringify pre-processor trick. */
+#define XSTR(d) STR(d)
+#define STR(s) #s
+
+/* Send every event to a debug log level. This is useful to debug new trace
+ * events without implementing them for a specific tracing framework. Note
+ * that the arguments are ignored since at this step we do not know the
+ * types and amount there is. */
+#define tor_trace(subsystem, name, args...) \
+  log_debug(LD_GENERAL, "Trace event \"" XSTR(name) "\" from subsystem \"" \
+                        XSTR(subsystem) "\" hit. (line " XSTR(__LINE__) ")")
+
+#else /* TOR_TRACE_LOG_DEBUG */
 
 /*
  * Reaching this point, we NOP every tracing call since it was not enabled
