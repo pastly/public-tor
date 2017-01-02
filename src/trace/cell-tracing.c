5a3b5bc301a2cca506a8132abfa7c62a1532f8ef
Matt Traudt <sirmatt@ksu.edu>
2017-01-02 13:58:09 -0500

    Rename CellTracingEveryNCells to trace_every_n; line length, indentation

--- src/trace/cell-tracing.c
+++ src/trace/cell-tracing.c
@@ -10,14 +10,16 @@
 /* In tracing mode, a cell data structure has an ID field.
  * This is a global counter indicating the next ID a cell should get. It is
  * incremented every time the inbuf trace event is hit and
- * cell_counter == CellTracingEveryNCells. */
+ * cell_counter == trace_every_n. */
 static uint32_t cell_next_id = 1;
+
 /* This value reduces the number of cells that get traced. We count cells until
- * the counter reaches CellTracingEveryNCells, at which point we increment and
+ * the counter reaches trace_every_n, at which point we increment and
  * assign the cell a non-zero id. We also attach a non-zero timestamp. This
  * cuts down on log spam. */
-static int CellTracingEveryNCells;
+static int trace_every_n;
 static int cell_counter = 0;
+
 /* Whether or not tracing is enabled. First it must be enabled at compile time,
  * then the torrc option enabled.
  */
@@ -34,12 +36,13 @@ struct cell_info {
 void cell_tracing_init()
 {
   tracing_enabled = get_options()->CellTracingEnabled;
-  CellTracingEveryNCells = get_options()->CellTracingEveryNCells;
-  log_info(LD_OR, "[cell-tracing] (re)init cell tracing. enabled: %d, n: %d",
-      tracing_enabled, CellTracingEveryNCells);
+  trace_every_n = get_options()->CellTracingEveryNCells;
+  log_info(LD_OR, "[cell-tracing] (re)init cell tracing. enabled: %" PRIi32
+	", n: %" PRIi32, tracing_enabled, trace_every_n);
 }
 
-void tor_trace_channel_tls_write_packed_cell(connection_t *conn, const packed_cell_t *cell, int wide_circ_ids)
+void tor_trace_channel_tls_write_packed_cell(connection_t *conn,
+  const packed_cell_t *cell, int wide_circ_ids)
 {
   if (!tracing_enabled) return;
   if (cell->id < 1) return;
@@ -79,18 +82,21 @@ void tor_trace_channel_tls_write_packed_cell(connection_t *conn, const packed_ce
   if (result < 0) ts.tv_sec = ts.tv_nsec = 0;
   int64_t diff = ( ((uint64_t)ts.tv_sec) *1000000000 + ts.tv_nsec ) -
           ( ((uint64_t)c_info->ts.tv_sec)*1000000000 + c_info->ts.tv_nsec );
-  log_info(LD_OR, "[cell-tracing] %" PRIu32 ".%" PRIu32 " %" PRIu32 ".%" PRIu32 " %" PRIi64 " id=%" PRIu32 " waiting in outbuf", c_info->ts.tv_sec, c_info->ts.tv_nsec, ts.tv_sec, ts.tv_nsec, diff, c_info->id);
+  log_info(LD_OR, "[cell-tracing] %" PRIu32 ".%" PRIu32 " %" PRIu32 ".%" PRIu32
+	" %" PRIi64 " id=%" PRIu32 " waiting in outbuf", c_info->ts.tv_sec,
+	c_info->ts.tv_nsec, ts.tv_sec, ts.tv_nsec, diff, c_info->id);
 }
 
 void tor_trace_connection_cell_inbuf(cell_t *cell, connection_t *conn)
 {
   if (!tracing_enabled) return;
-  if (++cell_counter >= CellTracingEveryNCells) {
+  if (++cell_counter >= trace_every_n) {
     int result = clock_gettime(CLOCK_REALTIME, &(cell->ts));
     if (result < 0) cell->ts.tv_sec = cell->ts.tv_nsec = 0;
     cell->id = cell_next_id++;
-    cell_counter -= CellTracingEveryNCells;
-    log_info(LD_OR, "[cell-tracing] %lu.%lu id=%" PRIu32, cell->ts.tv_sec, cell->ts.tv_nsec, cell->id);
+    cell_counter -= trace_every_n;
+    log_info(LD_OR, "[cell-tracing] %" PRIu32 ".%" PRIu32 " id=%" PRIu32,
+	  cell->ts.tv_sec, cell->ts.tv_nsec, cell->id);
   } else {
     cell->id = 0;
     cell->ts.tv_sec = cell->ts.tv_nsec = 0;
@@ -120,9 +126,12 @@ void tor_trace_connection_write_to_buf_flushed(connection_t *conn, int amount)
       int result = clock_gettime(CLOCK_REALTIME, &ts);
       if (result < 0)
         ts.tv_sec = ts.tv_nsec = 0;
-      int64_t diff = ( ((uint64_t)ts.tv_sec)         *1000000000 + ts.tv_nsec ) -
-                      ( ((uint64_t)c_info->ts.tv_sec)*1000000000 + c_info->ts.tv_nsec );
-      log_info(LD_OR, "[cell-tracing] %" PRIu32 ".%" PRIu32 " %" PRIu32 ".%" PRIu32 " %" PRIi64 " id=%" PRIu32 " written to kernel", c_info->ts.tv_sec, c_info->ts.tv_nsec, ts.tv_sec, ts.tv_nsec, diff, c_info->id);
+      int64_t diff = ( ((uint64_t)ts.tv_sec)*1000000000 + ts.tv_nsec ) -
+        ( ((uint64_t)c_info->ts.tv_sec)*1000000000 + c_info->ts.tv_nsec );
+      log_info(LD_OR, "[cell-tracing] %" PRIu32 ".%" PRIu32 " %" PRIu32
+        ".%" PRIu32 " %" PRIi64 " id=%" PRIu32 " written to kernel",
+        c_info->ts.tv_sec, c_info->ts.tv_nsec, ts.tv_sec, ts.tv_nsec, diff,
+        c_info->id);
       tor_free(c_info);
       MAP_DEL_CURRENT(k);
     }
--- src/trace/cell-tracing.h
+++ src/trace/cell-tracing.h
@@ -34,7 +34,8 @@ void cell_tracing_init();
 #define tor_trace_connection(name, ...) \
   tor_trace_connection_##name(__VA_ARGS__)
 
-void tor_trace_channel_tls_write_packed_cell(connection_t *conn, const packed_cell_t *cell, int wide_circ_ids);
+void tor_trace_channel_tls_write_packed_cell(connection_t *conn,
+  const packed_cell_t *cell, int wide_circ_ids);
 void tor_trace_connection_cell_inbuf(cell_t *cell, connection_t *conn);
 void tor_trace_connection_write_to_buf_flushed(connection_t *conn, int amount);
 
